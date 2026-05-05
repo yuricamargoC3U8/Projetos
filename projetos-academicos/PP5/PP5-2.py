29fb431f0deab05ac522f1bcb70403b4f9b32fb3
Yuri <yuri.camargo@sempreceub.com>
2026-05-05 15:28:17 -0300

    Add function to sum three numbers

---
+++ projetos-academicos/PP5/PP5-2.py
@@ -0,0 +1,8 @@
+def soma(n1, n2, n3):
+    adicao = n1 + n2 + n3
+    return adicao
+if __name__ == "__main__":
+    num1 = int(input("Digite primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+    num3 = int(input("Digite o terceiro número: "))
+    print(soma(num1, num2, num3))
