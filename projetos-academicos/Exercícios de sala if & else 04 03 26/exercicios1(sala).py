d6dfcae3ef3bf8a217fe0f9921367308fe1ee029
Yuri <yuri.camargo@sempreceub.com>
2026-05-05 15:05:15 -0300

    Add triangle area calculation script

---
+++ projetos-academicos/Exercícios de sala if & else 04 03 26/exercicios1(sala).py
@@ -0,0 +1,8 @@
+base= float(input("Valor da base: "))
+altura= float(input("Valor da altura: "))
+
+area = base*altura/2
+print("Base:", base)
+print("Altura:", altura)
+print("A área do triangulo é", area)
+print(f"Área:  {area:.2f}")
