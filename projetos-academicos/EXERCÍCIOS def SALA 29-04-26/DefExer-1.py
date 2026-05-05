c914ee6be2cd032ab5340d7c2e8803c289888140
Yuri <yuri.camargo@sempreceub.com>
2026-05-05 15:32:31 -0300

    Add DefExer-1.py with value display function

---
+++ projetos-academicos/EXERCÍCIOS def SALA 29-04-26/DefExer-1.py
@@ -0,0 +1,9 @@
+def mostra_valor(valor):
+    print(valor)
+if __name__ == '__main__':
+    mostra_valor(5)
+    mostra_valor(23.8)
+    v_inteiro = 43
+    mostra_valor(v_inteiro)
+    v_real = 37.4
+    mostra_valor(v_real)
