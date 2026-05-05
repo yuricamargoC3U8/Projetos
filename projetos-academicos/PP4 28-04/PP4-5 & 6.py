79393222685896f6a9b085754ed1fe0461eb1783
Yuri <yuri.camargo@sempreceub.com>
2026-05-05 15:25:12 -0300

    Add parts 5 and 6 to PP4-5 & 6.py

---
+++ projetos-academicos/PP4 28-04/PP4-5 & 6.py
@@ -0,0 +1,9 @@
+# parte 5:
+# for lado_esquerdo in range(7):
+#     for lado_direito in range(7):
+#         print(f"{lado_esquerdo} | {lado_direito}")
+
+# parte 6:
+for lado_esquerdo in range(7):
+    for lado_direito in range(lado_esquerdo, 7):
+        print(f"{lado_esquerdo} | {lado_direito}")
