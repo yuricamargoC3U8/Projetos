2db2f76a26f0742c64c727d18922c90d1ddeaac6
Yuri <yuri.camargo@sempreceub.com>
2026-05-05 15:09:05 -0300

    Add number comparison script
    Add a program to compare two numbers and display the larger one.

---
+++ projetos-academicos/PP1/PP1-4.py
@@ -0,0 +1,10 @@
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro: "))
+if n1 > n2:
+    print("O maior valor é:", n1 )
+elif n2 > n1:
+    print("O maior valor é: ", n2)
+else:
+    print("Os valores são iguais!", n1,  n2)
+
+print("Os números digitados eram:",n1 ,n2)
