09081100ebb1574392be249d69651ca9da3ed211
Yuri <yuri.camargo@sempreceub.com>
2026-05-05 15:32:45 -0300

    Create DefExer-2.py

---
+++ projetos-academicos/EXERCÍCIOS def SALA 29-04-26/DefExer-2.py
@@ -0,0 +1,7 @@
+def mostra_valor(v1, v2):
+    soma = v1 + v2
+    return soma
+if __name__ == '__main__':
+    num1 = int(input('Digite um valor: '))
+    num2 = int(input('Digite um valor: '))
+    print(mostra_valor(num1, num2))
